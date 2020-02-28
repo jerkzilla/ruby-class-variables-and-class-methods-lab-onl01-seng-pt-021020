96ed349cf88710a38dd4f2e5e3a50fdf8da734b7
John J Marshall Jr <johnjamesmarshalljr@gmail.com>
2020-02-28 14:20:24 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -26,6 +26,18 @@ attr_accessor :name, :artist, :genre
     @@artists.uniq
   end
 
+  def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre]
+      genre_count[genre] += 1
+    else
+      genre_count[genre] = 1
+    end
+  end
+  genre_count
+end
+
 
 
 end
