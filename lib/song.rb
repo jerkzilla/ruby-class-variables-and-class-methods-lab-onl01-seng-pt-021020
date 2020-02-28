a97600db3af24b25353566a6903d12364a1a2c3f
John J Marshall Jr <johnjamesmarshalljr@gmail.com>
2020-02-28 14:14:57 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,7 @@
 class Song
 attr_accessor :name, :artist, :genre
 
-  @@count = 0
+  @@song_count = 0
   @@artists = []
   @@genres = []
 
@@ -10,11 +10,11 @@ attr_accessor :name, :artist, :genre
     @genre = genre
     @artist = artist
     @@count += 1
-    @@artists << self
-    @@genres << self
+    @@artists << @artist
+    @@genres << @genre
   end
 
   def self.count
-    @@count
+    @@song_count
   end
 end
