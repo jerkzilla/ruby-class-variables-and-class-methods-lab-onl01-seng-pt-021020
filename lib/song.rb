fb6a0612d6e303d718bcae5bc41d72b931d13dea
John J Marshall Jr <johnjamesmarshalljr@gmail.com>
2020-02-28 13:51:05 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,15 +1,15 @@
 class Song
 attr_accessor :name, :artist, :genre
 
-  @count = 0
-  @artists = []
-  @genres = []
+  @@count = 0
+  @@artists = []
+  @@genres = []
 
   def initialize(name,artist,genre)
     @name = name
     @genre = genre
     @artist = artist
-    @count += 1
+    @@count += 1
   end
 
   def self.count
